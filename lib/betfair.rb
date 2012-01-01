6946b75df3268393cbff4d34287e5dde6b95c550
Matt Chadburn <matt@commuterjoy.co.uk>
2012-01-01 23:32:18 +0000

    turn off HTTPI warnings

--- lib/betfair.rb
+++ lib/betfair.rb
@@ -2,3 +2,7 @@ require 'savon'
 require 'betfair/version'
 require 'betfair/api'
 require 'betfair/helpers'
+
+# prevents rogue debug output from HTTPI
+# - http://stackoverflow.com/questions/5009838/httpi-tried-to-user-the-httpi-adapter-error-using-savon-soap-library
+HTTPI.log = false
