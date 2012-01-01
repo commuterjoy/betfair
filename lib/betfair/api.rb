e1e2698bd1ec7b48b72d6e5d5aea1762c33da820
Matt Chadburn <matt@commuterjoy.co.uk>
2012-01-01 09:18:30 +0000

    passed all requests through generic Betfair::Api::request method and assigned the response session_token to every successful request. this allows us to keep long running processes (Eg, market logging) running off a single Betfair::Api::login call

--- lib/betfair/api.rb
+++ lib/betfair/api.rb
@@ -3,80 +3,7 @@ module Betfair
   class API
     
     attr_accessor :session_token, :error_code
-    
-    def place_bet(exchange_id, market_id, runner_id, bet_type, price, size)		
-      bf_bet = { :marketId => market_id, :selectionId => runner_id, :betType => bet_type, :price => price, :size => size, :asianLineId => 0, :betCategoryType => 'E', :betPersistenceType => 'NONE', :bspLiability => 0 }      
-      response = exchange(exchange_id).request :bf, :placeBets do
-        soap.body = { 'bf:request' => { :header => api_request_header, :bets => { 'PlaceBets' => [bf_bet] } } }
-      end      
-      error_code = response.to_hash[:place_bets_response][:result][:error_code]
-  		return error_code == 'OK' ? response.to_hash[:place_bets_response][:result][:bet_results][:place_bets_result] : error_code  		
-    end
-
-    def cancel_bet(exchange_id, bet_id)
-      response = exchange(exchange_id).request :bf, :cancelBets do
-        soap.body = { 'bf:request' => { :header => api_request_header, :bets => { 'CancelBets' => [{ :betId => bet_id }] } } } # "CancelBets" has to be a string, not a symbol!
-      end		
-      error_code = response.to_hash[:cancel_bets_response][:result][:error_code]
-  		return error_code == 'OK' ? response.to_hash[:cancel_bets_response][:result][:bet_results][:cancel_bets_result] : error_code
-    end
-        
-  	def get_market(exchange_id, market_id)
-  		response = exchange(exchange_id).request :bf, :getMarket do
-  			soap.body = { 'bf:request' => { :header => api_request_header, :marketId => market_id } }
-  		end
-  		error_code = response.to_hash[:get_market_response][:result][:error_code]
-  		return error_code == 'OK' ? response.to_hash[:get_market_response][:result][:market] : error_code
-  	end
-    
-    def get_market_prices_compressed(exchange_id, market_id, currency_code = nil)
-      response = exchange(exchange_id).request :bf, :getMarketPricesCompressed do
-       soap.body = { 'bf:request' => { :header => api_request_header,  :marketId => market_id, :currencyCode => currency_code } }
-      end
-      error_code = response.to_hash[:get_market_prices_compressed_response][:result][:error_code]      
-      return error_code == 'OK' ? response.to_hash[:get_market_prices_compressed_response][:result][:market_prices] : error_code
-    end
-            
-    def get_all_markets(exchange_id, event_type_ids = nil, locale = nil, countries = nil, from_date = nil, to_date = nil)
-      response = exchange(exchange_id).request :bf, :getAllMarkets do
-        soap.body = { 'bf:request' => { :header => api_request_header, 
-                                        :eventTypeIds => { 'int' => event_type_ids }, 
-                                        :locale => locale, :countries => { 'country' => countries}, 
-                                        :fromDate => from_date, 
-                                        :toDate => to_date 
-                                      } 
-                    }
-      end            
-      error_code = response.to_hash[:get_all_markets_response][:result][:error_code]      
-      return error_code == 'OK' ? response.to_hash[:get_all_markets_response][:result][:market_data] : error_code      
-    end
-                  
-    def login(username, password, product_id, vendor_software_id, location_id, ip_address)
-      response = @global_service.request :bf, :login do 
-        soap.body = { 'bf:request' => { :username => username, 
-                                        :password => password, 
-                                        :productId => product_id, 
-                                        :vendorSoftwareId => vendor_software_id, 
-                                        :locationId => location_id, 
-                                        :ipAddress => ip_address 
-                                       } 
-                    }
-      end
-      
-      response_header = response.to_hash[:login_response][:result][:header]
-      @error_code = response_header[:error_code]
-      @session_token = (response_header[:error_code].to_s === 'OK') ? response_header[:session_token].to_s : nil
-    end
-      
-    def exchange(exchange_id)   
-      exchange_id == 2  ? @aus_service : @uk_service
-    end
-
-    def api_request_header      
-      { :client_stamp => 0, :session_token => @session_token }
-    end
-
-    # 3/ refactor later to read from base64 encoded ~/.betfair/auth
+  
   	def initialize(options = {})
       
       config = { :proxy => nil,
@@ -117,6 +44,89 @@ module Betfair
       end
       
   	end
+
+    def exchange(exchange_id)   
+      exchange_id == 2  ? @aus_service : @uk_service
+    end
+
+    def api_request_header      
+      { :client_stamp => 0, :session_token => @session_token }
+    end
+        
+    # generic interface between betfair API and our public methods
+    def request(exchange_id, method, body)
+      response = exchange(exchange_id).request :bf, method do
+        soap.body = body
+      end
+      obj = method.to_s.gsub(/([A-Z])/, '_\1').downcase + '_response' # derive the response object name - Eg, placeBets => place_bets_response
+      response_header = response.to_hash[obj.to_sym][:result][:header]
+      @session_token = (response_header[:error_code].to_s === 'OK') ? response_header[:session_token].to_s : nil
+      response
+    end
+    
+    def place_bet(exchange_id, market_id, runner_id, bet_type, price, size)
+      bf_bet = { :marketId => market_id, :selectionId => runner_id, :betType => bet_type, :price => price, :size => size, :asianLineId => 0, :betCategoryType => 'E', :betPersistenceType => 'NONE', :bspLiability => 0 }      
+      response = self.request(exchange_id, :placeBets, {
+          'bf:request' => { :header => api_request_header, :bets => { 'PlaceBets' => [bf_bet] } } }
+          )
+      @error_code = response.to_hash[:place_bets_response][:result][:error_code]
+  		return @error_code == 'OK' ? response.to_hash[:place_bets_response][:result][:bet_results][:place_bets_result] : @error_code
+    end
+
+    def cancel_bet(exchange_id, bet_id)
+      response = self.request(exchange_id, :cancelBets, {
+          'bf:request' => { :header => api_request_header, :bets => { 'CancelBets' => [{ :betId => bet_id }] } } }
+          )
+      @error_code = response.to_hash[:cancel_bets_response][:result][:error_code]
+  		return @error_code == 'OK' ? response.to_hash[:cancel_bets_response][:result][:bet_results][:cancel_bets_result] : @error_code
+    end
+        
+  	def get_market(exchange_id, market_id)
+      response = self.request(exchange_id, :getMarket, {
+          'bf:request' => { :header => api_request_header, :marketId => market_id } }
+          )
+  		@error_code = response.to_hash[:get_market_response][:result][:error_code]
+  		return @error_code == 'OK' ? response.to_hash[:get_market_response][:result][:market] : @error_code
+  	end
+    
+    def get_market_prices_compressed(exchange_id, market_id, currency_code = nil)
+      response = self.request(exchange_id, :getMarketPricesCompressed, {      
+        'bf:request' => { :header => api_request_header,  :marketId => market_id, :currencyCode => currency_code } }
+        )
+      @error_code = response.to_hash[:get_market_prices_compressed_response][:result][:error_code]      
+      return @error_code == 'OK' ? response.to_hash[:get_market_prices_compressed_response][:result][:market_prices] : @error_code
+    end
+            
+    def get_all_markets(exchange_id, event_type_ids = nil, locale = nil, countries = nil, from_date = nil, to_date = nil)
+      response = self.request(exchange_id, :getAllMarkets, {      
+        'bf:request' => { :header => api_request_header, 
+                                        :eventTypeIds => { 'int' => event_type_ids }, 
+                                        :locale => locale, :countries => { 'country' => countries}, 
+                                        :fromDate => from_date, 
+                                        :toDate => to_date 
+                                      } 
+                    }
+        )
+      @error_code = response.to_hash[:get_all_markets_response][:result][:error_code]      
+      return @error_code == 'OK' ? response.to_hash[:get_all_markets_response][:result][:market_data] : @error_code      
+    end
+                  
+    def login(username, password, product_id, vendor_software_id, location_id, ip_address)
+      response = @global_service.request :bf, :login do 
+        soap.body = { 'bf:request' => { :username => username, 
+                                        :password => password, 
+                                        :productId => product_id, 
+                                        :vendorSoftwareId => vendor_software_id, 
+                                        :locationId => location_id, 
+                                        :ipAddress => ip_address 
+                                       } 
+                    }
+      end
+      
+      response_header = response.to_hash[:login_response][:result][:header]
+      @error_code = response_header[:error_code]
+      @session_token = (response_header[:error_code].to_s === 'OK') ? response_header[:session_token].to_s : nil
+    end
       
   end
   
